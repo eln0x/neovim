76cefe74edd396ed710d4c548ee9aeb04a111cc3
yoda <fred@drakonix.net>
2022-09-18 01:43:54 +0200

    update nvim tree

--- lua/plug/nvtree.lua
+++ lua/plug/nvtree.lua
@@ -20,11 +20,47 @@ nvtree.setup({
         exclude = {},
     },
     renderer = {
+        root_folder_modifier = ":t",
         group_empty = true,
+        icons = {
+            glyphs = {
+                default = "",
+                symlink = "",
+                folder = {
+                    arrow_open = "",
+                    arrow_closed = "",
+                    default = "",
+                    open = "",
+                    empty = "",
+                    empty_open = "",
+                    symlink = "",
+                    symlink_open = "",
+                },
+                git = {
+                    unstaged = "",
+                    staged = "S",
+                    unmerged = "",
+                    renamed = "➜",
+                    untracked = "U",
+                    deleted = "",
+                    ignored = "◌",
+                },
+            },
+        },
     },
     update_focused_file = {
         enable = true,
         update_cwd = true,
+    },
+    diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+        },
     },
     view = {
         adaptive_size = false,
