987f3d4cc5c62aab4240f3b23879e4d1656b567b
yoda <fred@drakonix.net>
2022-09-18 01:35:47 +0200

    rework autocommands

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -2,14 +2,26 @@
 -- NeoVim autocommands
 --
 
--- file indent by type
-vim.cmd "autocmd FileType c,cpp,cxx,h set cindent"
-vim.cmd "autocmd FileType make setlocal noexpandtab"
+vim.cmd [[
+    augroup _general_settings
+        autocmd!
+        autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR>
+        autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200})
+        autocmd BufWinEnter * :set formatoptions-=cro
+    augroup end
+]]
 
--- file type
-vim.cmd "autocmd BufNewFile,BufRead *.lib set filetype=sh"
-vim.cmd "autocmd BufNewFile,BufRead *.source set filetype=sh"
-vim.cmd "autocmd BufNewFile,BufRead *.pp set filetype=puppet"
-vim.cmd "autocmd BufNewFile,BufRead *.inc set filetype=perl"
-vim.cmd "autocmd BufRead,BufNewFile *.tex set filetype=tex"
-vim.cmd "autocmd BufRead,BufNewFile *.cls set filetype=tex"
+vim.cmd [[
+    augroup _filetype_settings
+        autocmd!
+        autocmd FileType qf set nobuflisted
+        autocmd FileType c,cpp,cxx,h set cindent
+        autocmd FileType make setlocal noexpandtab
+        autocmd BufNewFile,BufRead *.lib set filetype=sh
+        autocmd BufNewFile,BufRead *.source set filetype=sh
+        autocmd BufNewFile,BufRead *.pp set filetype=puppet
+        autocmd BufNewFile,BufRead *.inc set filetype=perl
+        autocmd BufRead,BufNewFile *.tex set filetype=tex
+        autocmd BufRead,BufNewFile *.cls set filetype=tex
+    augroup end
+]]
