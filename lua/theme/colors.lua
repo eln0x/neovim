4e78f7b8d96db79933423d2c7cb2a6552ddac131
yoda <fred@drakonix.net>
2022-09-18 18:37:18 +0200

    update colors

--- lua/theme/colors.lua
+++ lua/theme/colors.lua
@@ -15,21 +15,6 @@ local opt = vim.opt
 opt.termguicolors = true            -- Set term gui colors
 opt.background = "dark"             -- Select dark background
 
--- telescope
-vim.cmd([[
-    hi TelescopePromptBorder guifg=#f2594b guibg=#161616
-    hi TelescopePromptNormal  guifg=#f2594b guibg=#161616
-    hi TelescopePromptTitle  guifg=#f2594b guibg=#161616
-    hi TelescopePromptPrefix  guifg=#f2594b guibg=#161616
-    hi TelescopePromptCounter  guifg=#f2594b guibg=#161616
-    hi TelescopePreviewTitle  guifg=#8bba7f guibg=#161616
-    hi TelescopePreviewBorder guifg=#8bba7f guibg=#161616
-    hi TelescopeResultsTitle  guifg=#8bba7f guibg=#161616
-    hi TelescopeResultsBorder guifg=#8bba7f guibg=#161616
-    hi TelescopeMatching guifg=#e9b143 guibg=#161616
-    hi TelescopeSelection guifg=#ffffff guibg=#32302f
-]])
-
 -- completion
 vim.cmd([[
     hi! CmpPmenu guibg=NONE
