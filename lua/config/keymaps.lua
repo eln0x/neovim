edca92900709afe081d00e792dce8a5008ef2e1b
yoda <fred@drakonix.net>
2024-06-30 14:14:20 +0200

    update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,7 +5,7 @@
 
 local vim = vim
 local delkey = vim.keymap.del
-local setkey = vim.keymap.set
+local map = vim.keymap.set
 local opts = { silent = true }
 
 -- Remap space as leader key
@@ -19,65 +19,72 @@ vim.g.mapleader = " "
 delkey("n", "<leader>|", opts)
 delkey("n", "<leader>-", opts)
 
--- Copy whole file
-setkey("n", "<C-c>", "<cmd> %y+ <cr>", opts)
-
--- Move text up and down
-setkey("n", "<A-k>", "<Esc>:m .-2<cr>==gi", opts)
-setkey("n", "<A-j>", "<Esc>:m .+1<cr>==gi", opts)
-
--- Navigate line
-setkey("n", "<C-a>", "^", opts)
-setkey("n", "<C-e>", "$", opts)
-
--- Navigate tabs
---setkey("n", "<C-t>", "<cmd>tabnew<cr>", opts)
---setkey("n", "<C-PageDown>", "<cmd>tabprev<cr>", opts)
---setkey("n", "<C-PageUp>", "<cmd>tabnext<cr>", opts)
-
--- Navigate windows
-setkey("n", "<C-w><Up>", "<cmd>wincmd k<cr>")
-setkey("n", "<C-w><Down>", "<cmd>wincmd j<cr>")
-setkey("n", "<C-w><Right>", "<cmd>wincmd l<cr>")
-setkey("n", "<C-w><Left>", "<cmd>wincmd h<cr>")
-setkey("n", "<leader>!", "<cmd>vsplit<cr>")
-
--- Navigate buffers
-setkey("n", "<leader>n", "<cmd>enew<cr>", opts)
-setkey("n", "<leader><Right>", "<cmd>bnext<cr>", opts)
-setkey("n", "<leader><Left>", "<cmd>bprev<cr>", opts)
-
 -- Toggles
-setkey("n", "<F1>", "<cmd>Alpha<cr>", opts)
-setkey("n", "<F2>", "<cmd>NvimTreeToggl<cr>", opts)
-setkey("n", "<F3>", "<cmd>TagbarToggle<cr>", opts)
-setkey("n", "<F4>", "<cmd>AerialToggle<cr>", opts)
-setkey("n", "<F5>", "<cmd>Mason<cr>", opts)
-setkey("n", "<F6>", "<cmd>NullLsInfo<cr>", opts)
+map("n", "<F1>", "<cmd>Alpha<cr>", opts)
+map("n", "<F2>", "<cmd>NvimTreeToggl<cr>", opts)
+map("n", "<F3>", "<cmd>AerialToggle<cr>", opts)
+map("n", "<F4>", "<cmd>Mason<cr>", opts)
+map("n", "<F5>", "<cmd>NullLsInfo<cr>", opts)
 
 -- Cmotion
 vim.g.comfortable_motion_no_default_key_mappings = 1
-setkey('n', '<C-Down>', '<cmd>call comfortable_motion#flick(100)<cr>', opts)
-setkey('n', '<C-Up>', '<cmd>call comfortable_motion#flick(-100)<cr>', opts)
-setkey('n', '<C-f>', '<cmd>call comfortable_motion#flick(200)<cr>', opts)
-setkey('n', '<C-b>', '<cmd>call comfortable_motion#flick(-200)<cr>', opts)
+map('n', '<C-Down>', '<cmd>call comfortable_motion#flick(100)<cr>', { desc = "Scroll Down", silent = true } )
+map('n', '<C-Up>', '<cmd>call comfortable_motion#flick(-100)<cr>', { desc = "Scroll Up", silent = true } )
 
+-- Navigate windows
+map("n", "<C-w><Up>", "<cmd>wincmd k<cr>", { desc = "Go to Upper Window", remap = true } )
+map("n", "<C-w><Down>", "<cmd>wincmd j<cr>", { desc = "Go to Lower Window", remap = true } )
+map("n", "<C-w><Right>", "<cmd>wincmd l<cr>", { desc = "Go to Right Window", remap = true } )
+map("n", "<C-w><Left>", "<cmd>wincmd h<cr>", { desc = "Go to Left Window", remap = true } )
+map("n", "<leader>!", "<cmd>vsplit<cr>")
+
+
+
+-- Navigate tabs
+map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
+map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
+map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
+map("n", "<leader><tab>n", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+map("n", "<leader><tab>c", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+map("n", "<leader><tab>p", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+
+-- Navigate buffers
+map("n", "<leader>n", "<cmd>enew<cr>", { desc = "New File", silent = true } )
+map("n", "<leader><Right>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "<leader><Left>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+
+-- Navigate line
+map("n", "<C-a>", "^", opts)
+map("n", "<C-e>", "$", opts)
+
+-- Copy whole file
+map("n", "<C-c>", "<cmd> %y+ <cr>", opts)
+
+-- Save file
+map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+-- Clear search with <esc>
+map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
+
+-- Move text up and down
+map("n", "<A-k>", "<Esc>:m .-2<cr>==gi", { desc = "Move Up", silent = true } )
+map("n", "<A-j>", "<Esc>:m .+1<cr>==gi", { desc = "Move Down", silent = true } )
 
 --
 -- Visual_mode = "v",
 --
 
 -- Move text up and down
-setkey("v", "<A-j>", ":m .+1<cr>==", opts)
-setkey("v", "<A-k>", ":m .-2<cr>==", opts)
+map("v", "<A-j>", ":m .+1<cr>==", { desc = "Move Up", silent = true } )
+map("v", "<A-k>", ":m .-2<cr>==", { desc = "Move Down", silent = true } )
 
 --
 -- Visual_block_mode = "x",
 --
 
 -- Move text up and down
-setkey("x", "<A-j>", ":move '>+1<cr>gv-gv", opts)
-setkey("x", "<A-k>", ":move '<-2<cr>gv-gv", opts)
+map("x", "<A-j>", ":move '>+1<cr>gv-gv", { desc = "Move Up", silent = true } )
+map("x", "<A-k>", ":move '<-2<cr>gv-gv", { desc = "Move Down", silent = true } )
 
 -- Insert_mode = "i",
 -- Term_mode = "t",
