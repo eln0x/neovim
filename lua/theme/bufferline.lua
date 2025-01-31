c91ec2eeddf755e110efa02e1c2805669034d2ef
yoda <fred@drakonix.net>
2025-01-31 02:32:58 +0100

    fix bufferline

--- lua/theme/bufferline.lua
+++ lua/theme/bufferline.lua
@@ -10,34 +10,46 @@ end
 
 bufferline.setup {
     options = {
-        numbers = "none",                       -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-        close_command = "bdelete! %d",          -- can be a string | function, see "Mouse actions"
-        right_mouse_command = "bdelete! %d",    -- can be a string | function, see "Mouse actions"
-        left_mouse_command = "buffer %d",       -- can be a string | function, see "Mouse actions"
-        middle_mouse_command = nil,             -- can be a string | function, see "Mouse actions"
+        always_show_bufferline = true,
+        enforce_regular_tabs = true,
+        numbers = "none",
+        close_command = "bdelete! %d",
+        right_mouse_command = "bdelete! %d",
+        left_mouse_command = "buffer %d",
+        middle_mouse_command = nil,
         buffer_close_icon = '',
         modified_icon = "●",
         close_icon = "",
         left_trunc_marker = "",
         right_trunc_marker = "",
         max_name_length = 30,
-        max_prefix_length = 30,                 -- prefix used when a buffer is de-duplicated
+        max_prefix_length = 30,
         tab_size = 21,
-        diagnostics = false,                    -- | "nvim_lsp" | "coc",
+        diagnostics = false,
         diagnostics_update_in_insert = false,
-        offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = "",
+                padding = 1
+            },
+            {
+                filetype = "neo-tree",
+                text = "Neo-tree",
+                highlight = "Directory",
+                text_align = "left",
+            },
+        },
         show_buffer_icons = true,
         show_buffer_close_icons = true,
         show_close_icon = true,
         show_tab_indicators = true,
-        persist_buffer_sort = true,             -- whether or not custom sorted buffers should persist
-        separator_style = "thin",               -- | "padded" | "thick" | "thin" | { 'any', 'any' },
+        persist_buffer_sort = true,
+        separator_style = "thin",
         indicator = {
             icon = '',
             style = 'icon',
         },
-        enforce_regular_tabs = true,
-        always_show_bufferline = true,
     },
     highlights = {
         fill = {
