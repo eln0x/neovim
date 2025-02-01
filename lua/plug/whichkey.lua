f9250c8e2fbd7daebb28529cdbc8654acb073283
yoda <fred@drakonix.net>
2025-02-01 12:51:16 +0100

    fix whichkey

--- lua/plug/whichkey.lua
+++ lua/plug/whichkey.lua
@@ -10,163 +10,122 @@ end
 
 local setup = {
     plugins = {
-        marks = false,                      -- shows a list of your marks on ' and `
-        registers = false,                   -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-        spelling = {
-            enabled = true,                 -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-            suggestions = 20,               -- how many suggestions should be shown in the list?
-        },
+        marks = false,
+        registers = false,
+        spelling = true,
         presets = {
-            operators = false,              -- adds help for operators like d, y, ... and registers them for motion / text object completion
-            motions = true,                 -- adds help for motions
-            text_objects = true,            -- help for text objects triggered after entering an operator
-            windows = true,                 -- default bindings on <c-w>
-            nav = true,                     -- misc bindings to work with windows
-            z = true,                       -- bindings for folds, spelling and others prefixed with z
-            g = false,                       -- bindings for prefixed with g
+            operators = false,
+            motions = true,
+            text_objects = true,
+            windows = true,
+            nav = true,
+            z = true,
+            g = false,
         },
-    },
-    key_labels = {},
-    icons = {
-        breadcrumb = "»",                   -- symbol used in the command line area that shows your active key combo
-        separator = "➜",                    -- symbol used between a key and it's label
-        group = "+",                        -- symbol prepended to a group
-    },
-    popup_mappings = {
-        scroll_down = "<c-d>",              -- binding to scroll down inside the popup
-        scroll_up = "<c-u>",                -- binding to scroll up inside the popup
-    },
-    window = {
-        border = "rounded",                 -- none, single, double, shadow
-        position = "bottom",                -- bottom, top
-        margin = { 1, 0, 1, 0 },            -- extra window margin [top, right, bottom, left]
-        padding = { 2, 2, 2, 2 },           -- extra window padding [top, right, bottom, left]
-        winblend = 0,
     },
     layout = {
-        height = { min = 4, max = 25 },     -- min and max height of the columns
-        width = { min = 20, max = 50 },     -- min and max width of the columns
-        spacing = 5,                        -- spacing between columns
-        align = "center",                   -- align columns left, center or right
+        spacing = 5,
+        align = "center",
     },
-    ignore_missing = true,                  -- enable this to hide mappings for which you didn't specify a label
-    show_help = true,                       -- show help message on the command line when the popup is visible
-    triggers = "auto",                      -- automatically setup triggers
-    triggers_blacklist = {
-        i = { "j", "k" },
-        v = { "j", "k" },
-    },
-    hidden = { "<silent>", "<cmd>", "<Cmd>", "<cr>", "call", "lua", "^:", "^ " },
 }
 which_key.setup(setup)
 
+-- Default mappings and groups
+local defaults = {
+    mode = { "n", "v" },
+    { "<leader>!", "<cmd>vsplit<cr>", desc = "Split Vertical" },
+    { "<leader>/", "<cmd>split<cr>", desc = "Split Horizontal" },
+    { "<leader><tab>", group = "tabs" },
+    --{ "<leader>b", group = "buffer" },
+    { "<leader>l", group = "code" },
+    { "<leader>f", group = "file/find" },
+    { "<leader>g", group = "git" },
+    { "<leader>gh", group = "hunks" },
+    { "<leader>q", group = "quit/session" },
+    --{ "<leader>s", group = "search" },
+    --{ "<leader>sn", group = "noice" },
+    { "<leader>t", group = "terminal" },
+    { "<leader>u", group = "ui" },
+    { "<leader>w", group = "windows" },
+    --{ "<leader>x", group = "diagnostics/quickfix" },
+    { "[", group = "prev" },
+    { "]", group = "next" },
+    { "g", group = "goto" },
+    { "gs", group = "surround" },
+    { "z", group = "fold" },
+}
+which_key.add(defaults)
+
 -- Leader related mappings
-local lead_opts = {
-    mode = "n",                             -- NORMAL mode
-    prefix = "<leader>",
-    buffer = nil,                           -- Global mappings. Specify a buffer number for buffer local mappings
-    silent = true,                          -- use `silent` when creating keymaps
-    noremap = true,                         -- use `noremap` when creating keymaps
-    nowait = true,                          -- use `nowait` when creating keymaps
+local leader = {
+    mode = { "n" },
+    { "<leader>b", "<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<cr>", desc = "Find Buffer", nowait = true, remap = false },
+    { "<leader>c", "<cmd>bdelete<cr>", desc = "Close Buffer", nowait = true, remap = false },
+    { "<leader>h", "<cmd>nohlsearch<cr>", desc = "No Highlight", nowait = true, remap = false },
+    --{ "<leader>i", "<cmd>IndentBlanklineToggle<cr>", desc = "Indent Line", nowait = true, remap = false },
+    { "<leader>p", "<cmd>lua require('telescope').extensions.projects.projects(require('telescope.themes').get_dropdown{previewer = false})<cr>", desc = "Find Projects", nowait = true, remap = false },
+    { "<leader>s", "<cmd>w<cr>", desc = "Save Buffer", nowait = true, remap = false },
 }
+which_key.add(leader)
 
-local lead_mappings = {
-    ["!"] = { "<cmd>vsplit<cr>", "Split Vertical" },
-    ["/"] = { "<cmd>split<cr>", "Split Horizontal" },
-    b = { "<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<cr>",  "Find Buffer" },
-    c = { "<cmd>bdelete<cr>", "Close Buffer" },
-    f = { "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",  "Find Files" },
-    h = { "<cmd>nohlsearch<cr>", "No Highlight" },
-    i = { "<cmd>IndentBlanklineToggle<cr>", "Indent Line" },
-    p = { "<cmd>lua require('telescope').extensions.projects.projects(require('telescope.themes').get_dropdown{previewer = false})<cr>", "Find Projects" },
-    q = { "<cmd>q<cr>", "Quit Neovim" },
-    w = { "<cmd>w<cr>", "Save Buffer" },
-    x = { "<cmd>source %<cr>", "Reload Conf" },
-}
-
-which_key.register(lead_mappings, lead_opts)
-
--- Telescope related mappings
-local f_map = {
-    b = { "<cmd>Telescope buffers theme=ivy<cr>", "Buffers" },
-    c = { "<cmd>Telescope commands theme=ivy<cr>", "Commands" },
-    f = { "<cmd>Telescope find_files theme=ivy<cr>",  "Files" },
-    g = { "<cmd>Telescope live_grep theme=ivy<cr>", "Grep" },
-    k = { "<cmd>Telescope keymaps theme=ivy<cr>", "Keymaps" },
-    m = { "<cmd>Telescope man_pages theme=ivy<cr>", "Man Pages" },
-    n = { "<cmd>Telescope notify theme=ivy<cr>", "Notifications" },
-    o = { "<cmd>Telescope git_status theme=ivy<cr>", "Changed" },
-    s = { "<cmd>lua require('telescope.builtin').colorscheme(require('telescope.themes').get_dropdown{previewer = false})<cr>",  "Colorschemes" },
-    p = { "<cmd>lua require('telescope').extensions.projects.projects(require('telescope.themes').get_dropdown{previewer = false})<cr>", "Projects" },
-}
-which_key.register( f_map, {
-    mode = "n",                             -- NORMAL mode
-    prefix = "f",
-    buffer = nil,                           -- Global mappings. Specify a buffer number for buffer local mappings
-    silent = true,                          -- use `silent` when creating keymaps
-    noremap = true,                         -- use `noremap` when creating keymaps
-    nowait = true,                          -- use `nowait` when creating keymaps
-})
-
--- Lsp related mappings
-local l_map = {
-    ["?"] = { "<cmd>lua vim.lsp.buf.hover()<cr>", "Hover Doc" },
-    ["a"] = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
-    ["d"] = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Goto Definition" },
-    ["D"] = { "<cmd>lua vim.lsp.buf.declaration()<cr>", "Goto Declaration" },
-    ["f"] = { "<cmd>lua vim.lsp.buf.format{async=true}<cr>", "Format" },
-    ["h"] = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "Signature Help" },
-    ["i"] = { "<cmd>lua vim.lsp.buf.implementation()<cr>", "Goto Implementation" },
-    ["n"] = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "Diagnostic Goto Next" },
-    ["o"] = { "<cmd>lua vim.diagnostic.open_float()<cr>", "Diagnostic Open" },
-    ["p"] = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Diagnostic Goto Prev" },
-    ["r"] = { "<cmd>lua vim.lsp.buf.references()<cr>", "References" },
-    ["s"] = { "<cmd>lua vim.lsp.buf.document_symbol()<cr>", "Document Symbols" },
-    ["S"] = { "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", "Workspace Symbols" },
-    ["t"] = { "<cmd>lua vim.lsp.buf.type_definition()<cr>", "Type definition" },
-    ["w"] = { "<cmd>Telescope diagnostics<cr>", "Workspace Diagnostics" },
-}
-which_key.register(l_map, {
-    mode = "n",                             -- NORMAL mode
-    prefix = "l",
-    buffer = nil,                           -- Global mappings. Specify a buffer number for buffer local mappings
-    silent = true,                          -- use `silent` when creating keymaps
-    noremap = true,                         -- use `noremap` when creating keymaps
-    nowait = true,                          -- use `nowait` when creating keymaps
-})
-
--- Git related mappings
-local g_map = {
-    j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
-    k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
-    l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
-    p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
-}
-which_key.register(g_map, {
-    mode = "n",                             -- NORMAL mode
-    prefix = "g",
-    buffer = nil,                           -- Global mappings. Specify a buffer number for buffer local mappings
-    silent = true,                          -- use `silent` when creating keymaps
-    noremap = true,                         -- use `noremap` when creating keymaps
-    nowait = true,                          -- use `nowait` when creating keymaps
-})
 
 -- Terminal related mappings
-local t_map = {
-    g = { "<cmd>lua GitToggle()<cr>", "Lazygit" },
-    n = { "<cmd>lua NodeToggle()<cr>", "Node" },
-    u = { "<cmd>lua NcduToggle()<cr>", "Ncdu" },
-    t = { "<cmd>lua HtopToggle()<cr>", "Htop" },
-    p = { "<cmd>lua PythonToggle()<cr>", "Python" },
-    f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
-    h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
-    v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
+local terminal = {
+    mode = { "n" },
+    { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Float", nowait = true, remap = false },
+    { "<leader>tg", "<cmd>lua GitToggle()<cr>", desc = "Lazygit", nowait = true, remap = false },
+    { "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "Horizontal", nowait = true, remap = false },
+    { "<leader>tn", "<cmd>lua NodeToggle()<cr>", desc = "Node", nowait = true, remap = false },
+    { "<leader>tp", "<cmd>lua PythonToggle()<cr>", desc = "Python", nowait = true, remap = false },
+    { "<leader>tt", "<cmd>lua HtopToggle()<cr>", desc = "Htop", nowait = true, remap = false },
+    { "<leader>tu", "<cmd>lua NcduToggle()<cr>", desc = "Ncdu", nowait = true, remap = false },
+    { "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "Vertical", nowait = true, remap = false },
 }
-which_key.register(t_map, {
-    mode = "n",                             -- NORMAL mode
-    prefix = "t",
-    buffer = nil,                           -- Global mappings. Specify a buffer number for buffer local mappings
-    silent = true,                          -- use `silent` when creating keymaps
-    noremap = true,                         -- use `noremap` when creating keymaps
-    nowait = true,                          -- use `nowait` when creating keymaps
-})
+which_key.add(terminal)
+
+-- Finder related mappings
+local finder = {
+    mode = { "n" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers", nowait = true, remap = false },
+    { "<leader>fc", "<cmd>Telescope commands<cr>", desc = "Commands", nowait = true, remap = false },
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Files", nowait = true, remap = false },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Grep", nowait = true, remap = false },
+    { "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps", nowait = true, remap = false },
+    { "<leader>fm", "<cmd>Telescope man_pages<cr>", desc = "Man Pages", nowait = true, remap = false },
+    { "<leader>fn", "<cmd>Telescope notify<cr>", desc = "Notifications", nowait = true, remap = false },
+    { "<leader>fo", "<cmd>Telescope git_status<cr>", desc = "Changed", nowait = true, remap = false },
+    { "<leader>fs", "<cmd>lua require('telescope.builtin').colorscheme(require('telescope.themes').get_dropdown{previewer = false})<cr>", desc = "Colorschemes", nowait = true, remap = false },
+    { "<leader>fp", "<cmd>lua require('telescope').extensions.projects.projects(require('telescope.themes').get_dropdown{previewer = false})<cr>", desc = "Projects", nowait = true, remap = false },
+}
+which_key.add(finder)
+
+-- Git related mappings
+local git = {
+    mode = { "n" },
+    { "<leader>gj", "<cmd>lua require 'gitsigns'.next_hunk()<cr>", desc = "Next Hunk", nowait = true, remap = false },
+    { "<leader>gk", "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", desc = "Prev Hunk", nowait = true, remap = false },
+    { "<leader>gl", "<cmd>lua require 'gitsigns'.blame_line()<cr>", desc = "Blame", nowait = true, remap = false },
+    { "<leader>gp", "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", desc = "Preview Hunk", nowait = true, remap = false },
+}
+which_key.add(git)
+
+-- Code related mappings
+local code = {
+    mode = { "n" },
+    { "<leader>l?", "<cmd>lua vim.lsp.buf.hover()<cr>", desc = "Hover Doc", nowait = true, remap = false },
+    { "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", desc = "Code Action", nowait = true, remap = false },
+    { "<leader>ld", "<cmd>lua vim.lsp.buf.definition()<cr>", desc = "Goto Definition", nowait = true, remap = false },
+    { "<leader>lD", "<cmd>lua vim.lsp.buf.declaration()<cr>", desc = "Goto Declaration", nowait = true, remap = false },
+    { "<leader>lf", "<cmd>lua vim.lsp.buf.format{async=true}<cr>", desc = "Format", nowait = true, remap = false },
+    { "<leader>lh", "<cmd>lua vim.lsp.buf.signature_help()<cr>", desc = "Signature Help", nowait = true, remap = false },
+    { "<leader>li", "<cmd>lua vim.lsp.buf.implementation()<cr>", desc = "Goto Implementation", nowait = true, remap = false },
+    { "<leader>ln", "<cmd>lua vim.diagnostic.goto_next()<cr>", desc = "Diagnostic Goto Next", nowait = true, remap = false },
+    { "<leader>lo", "<cmd>lua vim.diagnostic.open_float()<cr>", desc = "Diagnostic Open", nowait = true, remap = false },
+    { "<leader>lp", "<cmd>lua vim.diagnostic.goto_prev()<cr>", desc = "Diagnostic Goto Prev", nowait = true, remap = false },
+    { "<leader>lr", "<cmd>lua vim.lsp.buf.references()<cr>", desc = "References", nowait = true, remap = false },
+    { "<leader>ls", "<cmd>lua vim.lsp.buf.document_symbol()<cr>", desc = "Document Symbols", nowait = true, remap = false },
+    { "<leader>lS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", desc = "Workspace Symbols", nowait = true, remap = false },
+    { "<leader>lt", "<cmd>lua vim.lsp.buf.type_definition()<cr>", desc = "Type Definition", nowait = true, remap = false },
+    { "<leader>lw", "<cmd>Telescope diagnostics<cr>", desc = "Workspace Diagnostics", nowait = true, remap = false },
+}
+which_key.add(code)
