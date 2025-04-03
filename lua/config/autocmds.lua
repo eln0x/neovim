dd180b0d0a949e1f77aff63545d4660d5855edcf
yoda <fred@drakonix.net>
2025-04-03 11:37:53 +0200

    lazy rework

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,40 +1,76 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set:
 -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
 -- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-local vim = vim
+-- Create augroup function
+local function augroup(name)
+	return vim.api.nvim_create_augroup("draknet_" .. name, { clear = true })
+end
 
-vim.cmd [[
-    augroup _general_settings
-        autocmd!
-        autocmd BufWinEnter * :set formatoptions-=cro
-        autocmd BufWritePre * :%s/\s\+$//e
-        autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
-    augroup end
-]]
+-- Highlight on yank
+vim.api.nvim_del_augroup_by_name("lazyvim_highlight_yank")
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = augroup("highlight_yank"),
+	callback = function()
+		(vim.hl or vim.highlight).on_yank({
+			--higroup = "Visual",
+			timeout = 200,
+		})
+	end,
+})
 
-vim.cmd [[
-    augroup _filetype_settings
-        autocmd!
-        autocmd FileType qf set nobuflisted
-        autocmd FileType c,cpp,cxx,h set cindent
-        autocmd FileType make setlocal noexpandtab
-        autocmd BufNewFile,BufRead *.lib set filetype=sh
-        autocmd BufNewFile,BufRead *.source set filetype=sh
-        autocmd BufNewFile,BufRead *.pp set filetype=puppet
-        autocmd BufNewFile,BufRead *.inc set filetype=perl
-        autocmd BufRead,BufNewFile *.tex set filetype=tex
-        autocmd BufRead,BufNewFile *.cls set filetype=tex
-    augroup end
-]]
+-- Close some filetypes with <q>
+vim.api.nvim_del_augroup_by_name("lazyvim_close_with_q")
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup("close_with_q"),
+	pattern = {
+		"PlenaryTestPopup",
+		"checkhealth",
+		"dbout",
+		"gitsigns-blame",
+		"grug-far",
+		"help",
+		"lspinfo",
+		"neotest-output",
+		"neotest-output-panel",
+		"neotest-summary",
+		"notify",
+		"qf",
+		"spectre_panel",
+		"startuptime",
+		"tsplayground",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.schedule(function()
+			vim.keymap.set("n", "q", function()
+				vim.cmd("close")
+				pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+			end, {
+				buffer = event.buf,
+				silent = true,
+				desc = "Quit buffer",
+			})
+		end)
+	end,
+})
 
-vim.cmd [[
-    augroup __remember_folds
-        autocmd!
-        au BufWinLeave ?* mkview 1
-        au BufWinEnter ?* silent! loadview 1
-    augroup END
-]]
+-- Remove trailing white spaces
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	pattern = { "*" },
+	callback = function()
+		local save_cursor = vim.fn.getpos(".")
+		pcall(function()
+			vim.cmd([[%s/\s\+$//e]])
+		end)
+		vim.fn.setpos(".", save_cursor)
+	end,
+})
 
 -- vim: ts=4 sts=4 sw=4 et
