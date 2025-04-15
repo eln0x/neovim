667351760371a8f542ce9f7395de2b2346b6969f
yoda <fred@drakonix.net>
2025-04-15 02:06:31 +0200

    update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,7 +3,9 @@
 --if true then return {} end
 
 return {
+
     -- Theme creation helper
+    -- https://github.com/rktjmp/lush.nvim
     {
         'rktjmp/lush.nvim',
     },
@@ -27,15 +29,71 @@ return {
     -- Tokyonight
     {
         'folke/tokyonight.nvim',
-        opts = { style = "night" },
+        lazy = false,
+        opts = { style = "moon" },
     },
 
-    -- Lunar
+    -- Catppuccin
+    -- https://github.com/catppuccin/nvim
     {
-        'lunarvim/darkplus.nvim',
+        "catppuccin/nvim",
+        lazy = false,
+        name = "catppuccin",
+        opts = {
+            integrations = {
+                aerial = true,
+                alpha = true,
+                cmp = true,
+                dashboard = true,
+                flash = true,
+                fzf = true,
+                grug_far = true,
+                gitsigns = true,
+                headlines = true,
+                illuminate = true,
+                indent_blankline = { enabled = true },
+                leap = true,
+                lsp_trouble = true,
+                mason = true,
+                markdown = true,
+                mini = true,
+                native_lsp = {
+                    enabled = true,
+                    underlines = {
+                        errors = { "undercurl" },
+                        hints = { "undercurl" },
+                        warnings = { "undercurl" },
+                        information = { "undercurl" },
+                    },
+                },
+                navic = { enabled = true, custom_bg = "lualine" },
+                neotest = true,
+                neotree = true,
+                noice = true,
+                notify = true,
+                semantic_tokens = true,
+                snacks = true,
+                telescope = true,
+                treesitter = true,
+                treesitter_context = true,
+                which_key = true,
+            },
+        },
+        specs = {
+            {
+                "akinsho/bufferline.nvim",
+                optional = true,
+                opts = function(_, opts)
+                    if (vim.g.colors_name or ""):find("catppuccin") then
+                        opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+                    end
+                end,
+            },
+        },
     },
 
     -- Color highlighter
+    -- https://github.com/norcalli/nvim-colorizer.lua
     {
         'NvChad/nvim-colorizer.lua',
         init = function()
