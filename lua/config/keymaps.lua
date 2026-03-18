a1a0d731c0cdd6aa0c10c6a9158e7c931ded45d8
yoda <fred@drakonix.net>
2026-03-19 00:03:16 +0100

    feat(term): terminal config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,6 +6,13 @@
 local set = vim.keymap.set
 local del = vim.keymap.del
 
+-- Scroll inside terminal
+local function term_scroll(direction, count)
+    count = count or 5
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, true, true), "n", false)
+    vim.cmd("normal! " .. count .. direction)
+end
+
 -- Toggles
 set("n", "<F2>", "<cmd>Alpha<cr>", { silent = true })
 set("n", "<F3>", "<cmd>Mason<cr>", { silent = true })
@@ -28,12 +35,29 @@ set("n", "<leader><Right>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 set("n", "<leader><Left>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 set("n", "<leader>$", "<cmd>bdelete<cr>", { desc = "Del Buffer" })
 
+-- Navigate terminal
+set("t", "<C-w><Left>", [[<C-\><C-n><C-w>h]], { desc = "Go to Left Terminal", remap = true })
+set("t", "<C-w><Right>", [[<C-\><C-n><C-w>l]], { desc = "Go to Right Terminal", remap = true })
+set("t", "<C-w><Up>", [[<C-\><C-n><C-w>k]], { desc = "Go to Upper Terminal", remap = true })
+set("t", "<C-w><Down>", [[<C-\><C-n><C-w>j]], { desc = "Go to Lower Terminal", remap = true })
+
+-- Exit terminal
+set("t", "<C-Left>", "<C-\\><C-n>", { noremap = true, silent = true, desc = "Exit terminal mode" })
+set("t", "<C-Right>", "<C-\\><C-n>", { noremap = true, silent = true, desc = "Exit terminal mode" })
+
+-- Scroll terminal
+set("t", "<C-Up>", function() term_scroll("k", 5) end, { remap = true, silent = true, desc = "Scroll up 5 lines" })
+set("t", "<C-Down>", function() term_scroll("j", 5) end, { remap = true, silent = true, desc = "Scroll down 5 lines" })
+
 -- Copy whole file
 set("n", "<C-c>", "<cmd> %y+ <cr>", { silent = true })
 set("n", "<C-a>", "ggVG", { noremap = true, silent = true })
 
 -- Save file
 set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+-- Clear search with <esc>
+set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
 
 -- Comfortable motion
 vim.g.comfortable_motion_no_default_key_mappings = 1
@@ -42,8 +66,5 @@ del("n", "<C-Down>")
 set("n", "<C-Down>", "<cmd>call comfortable_motion#flick(100)<cr>", { desc = "Scroll Down", silent = true })
 del("n", "<C-Up>")
 set("n", "<C-Up>", "<cmd>call comfortable_motion#flick(-100)<cr>", { desc = "Scroll Up", silent = true })
-
--- Clear search with <esc>
-set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
 
 -- vim: ts=4 sts=4 sw=4 et
