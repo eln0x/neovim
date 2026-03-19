1ec6b994ee6f34a065f865b4fd29f89cf92f357f
yoda <fred@drakonix.net>
2026-03-19 01:43:58 +0100

    feat(keymap): add toggle for conform and huez

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,6 +17,8 @@ end
 set("n", "<F2>", "<cmd>Alpha<cr>", { silent = true })
 set("n", "<F3>", "<cmd>Mason<cr>", { silent = true })
 set("n", "<F4>", "<cmd>LspInfo<cr>", { silent = true })
+set("n", "<F5>", "<cmd>ConformInfo<cr>", { silent = true })
+set("n", "<F6>", "<cmd>HuezFavorites<cr>", { silent = true })
 
 -- Split windows
 del("n", "<leader>-")
